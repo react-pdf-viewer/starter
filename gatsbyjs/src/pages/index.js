616e53bf3dad3e3904cb973f498c4b418193d415
Phuoc Nguyen <me@phuoc.ng>
2021-09-02 09:16:13 +0700

    Add starter for gatsbyjs

---
+++ gatsbyjs/src/pages/index.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '../styles/styles.css';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+    return (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.9.359/build/pdf.worker.js">
+            <div
+                style={{
+                    height: '750px',
+                    width: '900px',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                }}
+            >
+                <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[defaultLayoutPluginInstance]} />
+            </div>
+        </Worker>
+    );
+};
+
+export default IndexPage;
